ca56fffc41e42b59c9aba47f06f17d5886f31b27
lucas <lucassilvab.17@gmail.com>
2021-09-26 22:27:25 -0300

    atualização

--- main.js
+++ main.js
@@ -27,13 +27,16 @@ for (const link of links) {
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', () => {
+function changeHeaderWhenScroll() {
+
+
     if (window.scrollY > navHeight) {
         header.classList.add('scroll')
     } else {
         header.classList.remove('scroll')
     }
-})
+
+}
 
 /* testimonials slider swiper carrousel */
 
@@ -59,7 +62,53 @@ scrollReveal.reveal(
     #about .image, #about .text, 
     #services header, #services .card, 
     #testimonials header, #testimonials .testimonials
-    #contact .text, #contact .links
+    #contact .text, #contact .links,
+    footer .brand, footer .social
     `,
     { interval: 100 }
 )
+
+/*Back to top button */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+
+
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for (const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && checkpointEnd) {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.add('active')
+        } else {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.remove('active')
+        }
+    }
+}
+
+/*When Scroll*/
+window.addEventListener('scroll', function () {
+    changeHeaderWhenScroll()
+    backToTop()
+})
+
